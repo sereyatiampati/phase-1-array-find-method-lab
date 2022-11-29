e2ea2c7de37c54a87ec72f52647019fdba4218ae
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-11-29 18:42:15 +0300

    array methods

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+function superbowlWin(arr) { 
+  for (const item of arr ){
+    if (find(item)===true){
+      return item.year
+    }
+  }
+}
+
+function find(item){
+  return item.result==='W'
+}
+// const win=record.find(superbowlWin)
+// console.log(win.year)
+console.log (superbowlWin(record))
